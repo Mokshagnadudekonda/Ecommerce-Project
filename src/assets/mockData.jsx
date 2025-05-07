d0677d3bfe47a42bbcc66b01c961095669009f3a
Mokshagnadudekonda <mokshagnadudekonda@gmail.com>
2025-05-07 13:04:32 +0530

    added data

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -1,3 +1,15 @@
+import bag from './images/bag.webp'
+import boots from './images/boots.webp'
+import cap from './images/cap.webp'
+import dove from './images/dove.webp'
+import headset from './images/headset.webp'
+import perfume from './images/perfume.webp'
+import sandle from './images/sandle.webp'
+import shirt from './images/shirt.webp'
+import toys from './images/toys.webp'
+import watch from './images/watch.webp'
+
+
 export const Categories = [
     "Electronics",
     "Fashion",
@@ -6,3 +18,66 @@ export const Categories = [
     "Sport",
     "Automotive"
 ];
+
+export const mockData = [
+    {
+        id: 1,
+        image: bag,
+        name: "Bag",
+        price: 959
+    },
+    {
+        id: 2,
+        image: boots,
+        name: "Boots",
+        price: 840
+    },
+    {
+        id: 3,
+        image: cap,
+        name: "Cap",
+        price: 250
+    },
+    {
+        id: 4,
+        image: dove,
+        name: "Dove Shampoo",
+        price: 350
+    },
+    {
+        id: 5,
+        image: headset,
+        name: "Head Set",
+        price: 1750
+    },
+    {
+        id: 6,
+        image: perfume,
+        name: "Perfume",
+        price: 750
+    },
+    {
+        id: 7,
+        image: sandle,
+        name: "Sandle",
+        price: 1120
+    },
+    {
+        id: 8,
+        image: shirt,
+        name: "Shirt",
+        price: 650
+    },
+    {
+        id: 9,
+        image: toys,
+        name: "Toys",
+        price: 1650
+    },
+    {
+        id: 10,
+        image: watch,
+        name: "Watch",
+        price: 2450
+    }
+]
