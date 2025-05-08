8194db46b019612ba73adfff876755e5e4afdf65
Mokshagnadudekonda <mokshagnadudekonda@gmail.com>
2025-05-08 10:51:03 +0530

    implemented cart features

--- src/Redux-toolkit/cartSlice.jsx
+++ src/Redux-toolkit/cartSlice.jsx
@@ -1,15 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    products: []
+    products: [],
+    totalQuantity: 0,
+    totalPrice: 0
 }
 
 const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
-
-    },
+        addToCart(state, action) {
+            const newItem = action.payload;
+            const itemIndex = state.products.find((item) => item.id === newItem.id);
+            if (itemIndex) {
+                itemIndex.quantity++;
+                itemIndex.price += newItem.price
+            } else {
+                state.products.push({
+                    id: newItem.id,
+                    name: newItem.name,
+                    image: newItem.image,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price
+                })
+            }
+            state.totalPrice += newItem.price;
+            state.totalQuantity++;
+        }
+    }
 })
-
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import { FaStar } from 'react-icons/fa'
+import { addToCart } from '../Redux-toolkit/cartSlice'
+import { useDispatch } from 'react-redux'
 
 const ProductCard = ({ product }) => {
+    const dispatch = useDispatch();
+    const handleAddToCart = (e, product) => {
+        e.stopPropagation();
+        e.preventDefault();
+        dispatch(addToCart(product));
+        alert("Added Sucessfully")
+    }
     return (
         <div className='bg-white p-4 shadow rounded relative transform transition-transform duration-300 hover:scale-105'>
             <img src={product.image} alt={product.name} className='w-full h-48 object-contain mb-4' />
@@ -14,7 +23,7 @@ const ProductCard = ({ product }) => {
                 <FaStar className='text-yellow-400' />
                 <FaStar className='text-gray-400' />
             </div>
-            <div className='absolute bottom-2 right-4 flex justify-center items-center w-8 h-8 bg-red-500 group text-white text-sm rounded-full hover:w-24 hover:bg-red-600'>
+            <div className='absolute bottom-2 right-4 flex justify-center items-center w-8 h-8 bg-red-500 group text-white text-sm rounded-full hover:w-24 hover:bg-red-600 transition-all duration-100' onClick={(e) => handleAddToCart(e, product)}>
                 <span className='group-hover:hidden'>+</span>
                 <span className='hidden group-hover:block'>Add to cart</span>
             </div>
